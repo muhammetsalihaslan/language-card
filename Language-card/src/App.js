390638d00b87c62577f2e480290914705387708b
salih <muhammetsalihaslan@gmail.com>
2022-06-28 18:38:36 +0300

    first

---
+++ Language-card/src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.scss';
+import Component from './components/Component';
+
+
+
+function App() {
+  return (
+    <div>
+      <Component/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Language-card/src/components/Component.jsx
@@ -0,0 +1,22 @@
+
+const Component = () => {
+    return(
+        <div>
+            <header><img src="" alt="" /></header>
+            <div>
+                <p>Languages</p>
+                <div className="cards">
+                    <img src="" alt="" />
+                    <div className="title">
+                        <p></p>
+                    </div>
+                        
+
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Component;
